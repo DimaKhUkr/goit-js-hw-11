d8d09ef0123ce5051bdc14d1aa919d30bff5439c
Dmytro Potapenko <dima.lmkh@gmail.com>
2023-02-04 22:14:17 +0200

    lib start

--- src/index.js
+++ src/index.js
@@ -3,3 +3,25 @@ import { fetchCountries } from './fetchCountries.js';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
+import axios from 'axios';
+
+let userRequestData = 'ukraine';
+let pageNumber = 1;
+
+async function getPic() {
+  const response = await axios({
+    url: 'https://pixabay.com/api/',
+    params: {
+      key: '33377492-476d22b77d4b85ba3622e340f',
+      q: `${userRequestData}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: pageNumber,
+      per_page: 30,
+    },
+  });
+  return response.data;
+}
+
+getPic().then(console.log);
